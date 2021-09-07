f17895d68dc933f62b762460d9472a983278cf3c
ty-crabtree <tyler.crabtree@smarsh.com>
2021-09-07 14:19:59 -0700

    Add to splunk lib

--- splunk_api.py
+++ splunk_api.py
@@ -1,8 +1,6 @@
 import os
 import sys
 
-
-
 try:
     import splunklib
     import splunklib.client as connect
@@ -29,20 +27,29 @@ class Splunk_API:
         PORT = 8089
         USERNAME = ""
         PASSWORD = ""
-        service = splunklib.client.connect(host=HOST, port=PORT, username=USERNAME, password=PASSWORD)
+        OWNER = "admin"
+        service = splunklib.client.connect(host=HOST, port=PORT, username=USERNAME, password=PASSWORD, owner=OWNER)
         self.apps = service.apps
 
     def get_apps(self):
         for app in self.service.apps:
             print(app)
 
+    def get_ready_apps(self):
+        ready_apps = []
+        for ready_app in self.service.apps.check_apps_ready():
+            print(ready_app)
+            ready_apps.append(ready_app)
+        return ready_apps
+
+    def delete_app(self,application):
+        self.apps.delete(application)
 
     def controller(self):
         print("Setting up connection")
-        self.setup_connection()
+        self.setup_connection()  # Can't get an established connection
         print("Getting applications")
         self.get_apps()
-
 
 
 if __name__ == '__main__':
